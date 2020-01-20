b9ac373d1fe6819174aeb331ae53abd8708bc5af
sangeetha <16tuit124@gmail.com>
2020-01-20 11:52:03 +0530

    Loginn

---
+++ app/src/main/java/com/example/furniturefinal/activities/DisplayActivity.java
@@ -0,0 +1,16 @@
+package com.example.furniturefinal.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.furniturefinal.R;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+    }
+}
